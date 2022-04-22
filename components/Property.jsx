da14e8f4f8dd36f32418222ae7dad0abf3eb99f5
Quentin Chin <q.chin@hotmail.com>
2022-04-21 23:55:35 -0400

    Update property component

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,13 +1,26 @@
-// import Link from 'next/link'
-// import Image from 'next/image'
-// import {Box, Flex, Text, Avatar} from '@chakra-ui/react'
-// import {FaBed, FaBath } from 'react-icons/fa'
-// import {BsGridFill} from 'react-icons/bs'
-// import {GoVerified} from 'react-icons/goverified'
-// import millify from 'millify'
+import Link from "next/link";
+import Image from "next/image";
+import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+import { FaBed, FaBath } from "react-icons/fa";
+import { BsGridFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import millify from "millify";
 
-const Property = () => {
-  return <h1>Property</h1>;
+const Property = ({
+  property: {
+    coverPhoto,
+    price,
+    rentFrequency,
+    rooms,
+    title,
+    baths,
+    area,
+    agency,
+    isVerified,
+    externalId,
+  },
+}) => {
+  return <Link href={`/property/${externalId}`}>{title}</Link>;
 };
 
 export default Property;
