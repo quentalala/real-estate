0a00d9e4ff3b8903f860aa8866e748f7790e6e62
Quentin Chin <q.chin@hotmail.com>
2022-05-18 23:56:20 -0400

    Add layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Router from "next/router";
+import Head from "next/head";
+import nProgress from "nprogress";
+// nProgress will be used for the loading bar
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
